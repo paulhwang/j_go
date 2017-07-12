f8f9be3bddb7587eb661b3ed52c51f42f34e11b7
paulhwang <phwang@gmail.com>
2017-07-12 05:26:02 +0000

    t

--- js/phwang_folder/phwang_link_storage.js
+++ js/phwang_folder/phwang_link_storage.js
@@ -5,51 +5,24 @@
 
 function PhwangLinkStorageObject(phwang_link_object_val) {
     "use strict";
+    this.storage = function () {return localStorage;};
+    this.init__ = function (phwang_link_object_val) {this.thePhwangLinkObject = phwang_link_object_val;};
 
-    this.storage = function () {
-        return localStorage;
+    this.resetLinkStorage = function () {
+        this.resetLinkId();
+        this.resetMyName();
+        this.resetPassWord();
     };
 
-    this.init__ = function (phwang_link_object_val) {
-        this.thePhwangLinkObject = phwang_link_object_val;
-        this.debug(true, "init__", "");
-    };
-
-    this.objectName = function () {
-        return "PhwangLinkStorageObject";
-    };
-
-    this.phwangLinkObject = function () {
-        return this.thePhwangLinkObject;
-    };
-
-    this.phwangObject = function () {
-        return this.phwangLinkObject().phwangObject();
-    };
-
-    this.myName = function () {
-        return this.storage().my_name;
-    };
-
-    this.setMyName = function (val) {
-        this.storage().my_name = val;
-    };
-
-    this.passWord = function () {
-        return this.thePassWord;
-    };
-
-    this.setPassWord = function (val) {
-        this.thePassWord = val;
-    };
-
-    this.linkId = function () {
-        return this.storage().link_id;
-    };
-
-    this.setLinkId = function (val) {
-        this.storage().link_id = val;
-    };
+    this.myName = function () {return this.storage().my_name;};
+    this.setMyName = function (val) {this.storage().my_name = val;};
+    this.resetMyName = function () {this.setMyName("");};
+    this.passWord = function () {return this.thePassWord;};
+    this.setPassWord = function (val) {this.thePassWord = val;};
+    this.resetPassWord = function () {this.setPassWord("");};
+    this.linkId = function () {return this.storage().link_id;};
+    this.setLinkId = function (val) {this.storage().link_id = val;};
+    this.resetLinkId = function () {this.setLinkId("");};
 
     this.serverIp = function () {
         return this.storage().server_ip;
@@ -84,37 +57,11 @@ function PhwangLinkStorageObject(phwang_link_object_val) {
         this.setServerPort(window.location.port);
     };
 
-    this.resetLinkStorage = function () {
-        this.resetLinkId();
-        this.resetMyName();
-        this.resetPassWord();
-    };
-
-    this.resetLinkId = function () {
-        this.setLinkId("");
-    };
-
-    this.resetMyName = function () {
-        this.setMyName("");
-    };
-
-    this.resetPassWord = function () {
-        this.setPassWord("");
-    };
-
-    this.debug = function (debug_val, str1_val, str2_val) {
-        if (debug_val) {
-            this.logit(str1_val, str2_val);
-        }
-    };
-
-    this.logit = function (str1_val, str2_val) {
-        return this.phwangObject().LOG_IT(this.objectName() + "." + str1_val, str2_val);
-    };
-
-    this.abend = function (str1_val, str2_val) {
-        return this.phwangObject().ABEND(this.objectName() + "." + str1_val, str2_val);
-    };
-
+    this.objectName = function () {return "PhwangLinkStorageObject";};
+    this.phwangLinkObject = function () {return this.thePhwangLinkObject;};
+    this.phwangObject = function () {return this.phwangLinkObject().phwangObject();};
+    this.debug = function (debug_val, str1_val, str2_val) {if (debug_val) {this.logit(str1_val, str2_val);}};
+    this.logit = function (str1_val, str2_val) {return this.phwangObject().LOG_IT(this.objectName() + "." + str1_val, str2_val);};
+    this.abend = function (str1_val, str2_val) {return this.phwangObject().ABEND(this.objectName() + "." + str1_val, str2_val);};
     this.init__(phwang_link_object_val);
 }
