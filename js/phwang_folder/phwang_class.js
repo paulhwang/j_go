b88e3290e1532d2236f788fbe505b43e0b7bd7d4
paulhwang <phwang@gmail.com>
2017-07-12 02:47:38 +0000

    t

--- js/phwang_folder/phwang_class.js
+++ js/phwang_folder/phwang_class.js
@@ -6,50 +6,16 @@
 function PhwangClass (root_val) {
     "use strict";
 
+    this.init__ = function (root_val) {
+        this.theRootObject = root_val;
+        this.debug(true, "init__", "");
+    };
+
     this.initObject = function () {
         this.thePhwangAjaxObject = new PhwangAjaxClass(this);
         this.thePhwangLinkObject = new PhwangLinkClass(this);
         this.thePhwangSessionObject = new PhwangSessionClass(this.phwangLinkObject());
-
         this.debug(true, "initObject", "");
-    };
-
-    this.init__ = function (root_val) {
-        this.theRootObject = root_val;
-
-        this.debug(true, "init__", "");
-    };
-
-    this.objectName = function () {
-        return "PhwangClass";
-    };
-
-    this.rootObject = function () {
-        return this.theRootObject;
-    };
-
-    this.phwangAjaxStorageObject = function () {
-        return this.thePhwangAjaxStorageObject;
-    };
-
-    this.phwangLinkStorageObject = function () {
-        return this.thePhwangLinkStorageObject;
-    };
-
-    this.phwangSessionStorageObject = function () {
-        return this.thePhwangSessionStorageObject;
-    };
-
-    this.phwangAjaxObject = function () {
-        return this.thePhwangAjaxObject;
-    };
-
-    this.phwangLinkObject = function () {
-        return this.thePhwangLinkObject;
-    };
-
-    this.phwangSessionObject = function () {
-        return this.thePhwangSessionObject;
     };
 
     this.decodeNumber = function (input_val, size_val) {
@@ -63,24 +29,15 @@ function PhwangClass (root_val) {
         return output;
     };
 
-    this.debug = function (debug_val, str1_val, str2_val) {
-        if (debug_val) {
-            this.logit(str1_val, str2_val);
-        }
-    };
-
-    this.logit = function (str1_val, str2_val) {
-        return this.LOG_IT(this.objectName() + "." + str1_val, str2_val);
-    };
-
-    this.abend = function (str1_val, str2_val) {
-        return this.ABEND(this.objectName() + "." + str1_val, str2_val);
-    };
-
-    this.LOG_IT = function (str1_val, str2_val) {
-        window.console.log(str1_val + "() " + str2_val);
-    };
-
+    this.objectName = function () {return "PhwangClass";};
+    this.rootObject = function () {return this.theRootObject;};
+    this.phwangAjaxObject = function () {return this.thePhwangAjaxObject;};
+    this.phwangLinkObject = function () {return this.thePhwangLinkObject;};
+    this.phwangSessionObject = function () {return this.thePhwangSessionObject;};
+    this.debug = function (debug_val, str1_val, str2_val) {if (debug_val) {this.logit(str1_val, str2_val);}};
+    this.logit = function (str1_val, str2_val) {return this.LOG_IT(this.objectName() + "." + str1_val, str2_val);};
+    this.abend = function (str1_val, str2_val) {return this.ABEND(this.objectName() + "." + str1_val, str2_val);};
+    this.LOG_IT = function (str1_val, str2_val) {window.console.log(str1_val + "() " + str2_val);};
     this.ABEND = function (str1_val, str2_val) {
         window.console.log("***ABEND*** " + str1_val + "() " + str2_val);
         window.alert("***ABEND*** " + str1_val + "() " + str2_val);
