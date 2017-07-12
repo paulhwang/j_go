10f8468938c0bc705a5c294fcba9d7bd19d8926c
paulhwang <phwang@gmail.com>
2017-07-12 03:50:03 +0000

    t

--- js/go_game/go_login_page/login_root.js
+++ js/go_game/go_login_page/login_root.js
@@ -10,65 +10,32 @@ function LoginRootObject() {
         this.thePhwangObject = new PhwangClass(this);
         this.phwangObject().initObject();
         this.theHtmlObject = new LoginHtmlObject(this);
+        this.theAjaxObject = new LoginAjaxClass(this);
         this.debug(true, "init__", "myName=" + this.phwangLinkObject().myName() + " linkId=" + this.phwangLinkObject().linkId());
 
         this.phwangLinkObject().setHttpInfo();
-    };
-
-    this.objectName = function () {
-        return "LoginRootObject";
     };
 
     this.gotoNextPage = function () {
         window.open(this.phwangLinkObject().serverHttpHeader() + "go_config.html", "_self")
     };
 
-    this.phwangObject = function () {
-        return this.thePhwangObject;
-    };
-
-    this.phwangAjaxObject = function () {
-        return this.phwangObject().phwangAjaxObject();
-    };
-
-    this.phwangLinkObject = function () {
-        return this.phwangObject().phwangLinkObject();
-    };
-
-    this.phwangSessionObject = function () {
-        return this.phwangObject().phwangSessionObject();
-    };
-
-    this.htmlObject = function () {
-        return this.theHtmlObject;
-    };
-
     this.receiveSetupLinkResponse = function () {
         this.gotoNextPage();
     };
 
-    this.debug = function (debug_val, str1_val, str2_val) {
-        if (debug_val) {
-            this.logit(str1_val, str2_val);
-        }
-    };
-
-    this.logit = function (str1_val, str2_val) {
-        return this.logit_(this.objectName() + "." + str1_val, str2_val);
-    };
-
-    this.abend = function (str1_val, str2_val) {
-        return this.abend_(this.objectName() + "." + str1_val, str2_val);
-    };
-
-    this.logit_ = function (str1_val, str2_val) {
-        this.phwangObject().LOG_IT(str1_val, str2_val);
-    };
-
-    this.abend_ = function (str1_val, str2_val) {
-        this.phwangObject().ABEND(str1_val, str2_val);
-    };
-
+    this.objectName = function () {return "LoginRootObject";};
+    this.phwangObject = function () {return this.thePhwangObject;};
+    this.phwangAjaxObject = function () {return this.phwangObject().phwangAjaxObject();};
+    this.phwangLinkObject = function () {return this.phwangObject().phwangLinkObject();};
+    this.phwangSessionObject = function () {return this.phwangObject().phwangSessionObject();};
+    this.htmlObject = function () {return this.theHtmlObject;};
+    this.ajaxObject = function () {return this.theAjaxObject;};
+    this.debug = function (debug_val, str1_val, str2_val) {if (debug_val) {this.logit(str1_val, str2_val);}};
+    this.logit = function (str1_val, str2_val) {return this.logit_(this.objectName() + "." + str1_val, str2_val);};
+    this.abend = function (str1_val, str2_val) {return this.abend_(this.objectName() + "." + str1_val, str2_val);};
+    this.logit_ = function (str1_val, str2_val) {this.phwangObject().LOG_IT(str1_val, str2_val);};
+    this.abend_ = function (str1_val, str2_val) {this.phwangObject().ABEND(str1_val, str2_val);};
     this.init__();
 }
 
