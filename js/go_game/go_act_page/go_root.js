9697a68764bc8242080c60f201099c5b8fff9e90
paulhwang <phwang@gmail.com>
2017-07-12 04:35:30 +0000

    t

--- js/go_game/go_act_page/go_root.js
+++ js/go_game/go_act_page/go_root.js
@@ -29,78 +29,24 @@ function GoPlayRootObject() {
         this.debug(true, "init__", "boardSize=" + this.configStorageObject().boardSize() + " stoneColor=" + this.configStorageObject().stoneColor() + " komi=" + this.configStorageObject().komi() + " handicap=" + this.configStorageObject().handicap());
     };
 
-    this.objectName = function () {
-        return "GoPlayRootObject";
-    };
-
-    this.phwangObject = function () {
-        return this.thePhwangObject;
-    };
-
-    this.phwangAjaxObject = function () {
-        return this.phwangObject().phwangAjaxObject();
-    };
-
-    this.phwangLinkObject = function () {
-        return this.phwangObject().phwangLinkObject();
-    };
-
-    this.phwangSessionObject = function () {
-        return this.phwangObject().phwangSessionObject();
-    };
-
+    this.objectName = function () {return "GoPlayRootObject";};
+    this.phwangObject = function () {return this.thePhwangObject;};
+    this.phwangAjaxObject = function () {return this.phwangObject().phwangAjaxObject();};
+    this.phwangLinkObject = function () {return this.phwangObject().phwangLinkObject();};
+    this.phwangSessionObject = function () {return this.phwangObject().phwangSessionObject();};
     this.ajaxObject = function () {return this.theAjaxObject;};
     this.htmlObject = function () {return this.theHtmlObject;};
-
-    this.configStorageObject = function () {
-        return this.theConfigStorageObject;
-    };
-    this.inputObject = function () {
-        return this.theInputObject;
-    };
-
-    this.displayObject = function () {
-        return this.theDisplayObject;
-    };
-
-    this.configObject = function () {
-        return this.theConfigObject;
-    };
-
-    this.boardObject = function () {
-        return this.theBoardObject;
-    };
-
-    this.gameObject = function () {
-        return this.theGameObject;
-    };
-
-    this.portObject = function () {
-        return this.thePortObject;
-    };
-
-    this.debug = function (debug_val, str1_val, str2_val) {
-        if (debug_val) {
-            this.logit(str1_val, str2_val);
-        }
-    };
-
-    this.logit = function (str1_val, str2_val) {
-        this.logit_(this.objectName() + "." + str1_val, str2_val);
-    };
-
-    this.abend = function (str1_val, str2_val) {
-        this.abend_(this.objectName() + "." + str1_val, str2_val);
-    };
-
-    this.logit_ = function (str1_val, str2_val) {
-        this.phwangObject().LOG_IT(str1_val, str2_val);
-    };
-
-    this.abend_ = function (str1_val, str2_val) {
-        this.phwangObject().ABEND(str1_val, str2_val);
-    };
-
+    this.configStorageObject = function () {return this.theConfigStorageObject;};
+    this.inputObject = function () {return this.theInputObject;};
+    this.displayObject = function () {return this.theDisplayObject;};
+    this.configObject = function () {return this.theConfigObject;};
+    this.boardObject = function () {return this.theBoardObject;};
+    this.gameObject = function () {return this.theGameObject;};
+    this.portObject = function () {return this.thePortObject;};
+    this.debug = function (debug_val, str1_val, str2_val) {if (debug_val) {this.logit(str1_val, str2_val);}};
+    this.logit = function (str1_val, str2_val) {this.logit_(this.objectName() + "." + str1_val, str2_val);};
+    this.abend = function (str1_val, str2_val) {this.abend_(this.objectName() + "." + str1_val, str2_val);};
+    this.logit_ = function (str1_val, str2_val) {this.phwangObject().LOG_IT(str1_val, str2_val);};
+    this.abend_ = function (str1_val, str2_val) {this.phwangObject().ABEND(str1_val, str2_val);};
     this.init__();
 }
-
--- js/go_game/go_config_page/config_root.js
+++ js/go_game/go_config_page/config_root.js
@@ -17,38 +17,18 @@ function ConfigRootObject() {
         this.debug(true, "init__", "myName=" + this.phwangLinkObject().myName() + " linkId=" + this.phwangLinkObject().linkId());
     };
 
-    this.objectName = function () {
-        return "ConfigRootObject";
-    };
-
     this.gotoNextPage = function () {
         window.open(this.phwangLinkObject().serverHttpHeader() + "go_act.html", "_self")
     };
 
-    this.phwangObject = function () {
-        return this.thePhwangObject;
-    };
-
-    this.phwangAjaxObject = function () {
-        return this.phwangObject().phwangAjaxObject();
-    };
-
-    this.phwangLinkObject = function () {
-        return this.phwangObject().phwangLinkObject();
-    };
-
-    this.phwangSessionObject = function () {
-        return this.phwangObject().phwangSessionObject();
-    };
-
-    this.configStorageObject = function () {
-        return this.theConfigStorageObject;
-    };
+    this.objectName = function () {return "ConfigRootObject";};
+    this.phwangObject = function () {return this.thePhwangObject;};
+    this.phwangAjaxObject = function () {return this.phwangObject().phwangAjaxObject();};
+    this.phwangLinkObject = function () {return this.phwangObject().phwangLinkObject();};
+    this.phwangSessionObject = function () {return this.phwangObject().phwangSessionObject();};
+    this.configStorageObject = function () {return this.theConfigStorageObject;};
     this.ajaxObject = function () {return this.theAjaxObject;};
-
-    this.htmlObject = function () {
-        return this.theHtmlObject;
-    };
+    this.htmlObject = function () {return this.theHtmlObject;};
 
     this.receiveSetupSessionResponse = function () {
         this.gotoNextPage();
@@ -58,32 +38,14 @@ function ConfigRootObject() {
         this.gotoNextPage();
     };
 
-    this.debug = function (debug_val, str1_val, str2_val) {
-        if (debug_val) {
-            this.logit(str1_val, str2_val);
-        }
-    };
-
     this.mallocLinkObject = function (name_val, id_val) {
         /* nothing is done here */
     };
 
-    this.logit = function (str1_val, str2_val) {
-        return this.logit_(this.objectName() + "." + str1_val, str2_val);
-    };
-
-    this.abend = function (str1_val, str2_val) {
-        return this.abend_(this.objectName() + "." + str1_val, str2_val);
-    };
-
-    this.logit_ = function (str1_val, str2_val) {
-        this.phwangObject().LOG_IT(str1_val, str2_val);
-    };
-
-    this.abend_ = function (str1_val, str2_val) {
-        this.phwangObject().ABEND(str1_val, str2_val);
-    };
-
+    this.debug = function (debug_val, str1_val, str2_val) {if (debug_val) {this.logit(str1_val, str2_val);}};
+    this.logit = function (str1_val, str2_val) {return this.logit_(this.objectName() + "." + str1_val, str2_val);};
+    this.abend = function (str1_val, str2_val) {return this.abend_(this.objectName() + "." + str1_val, str2_val);};
+    this.logit_ = function (str1_val, str2_val) {this.phwangObject().LOG_IT(str1_val, str2_val);};
+    this.abend_ = function (str1_val, str2_val) {this.phwangObject().ABEND(str1_val, str2_val);};
     this.init__();
 }
-
