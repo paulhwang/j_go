94561654ca19a4a756f739330a90207d0b2976a7
paulhwang <phwang@gmail.com>
2017-07-14 03:33:17 +0000

    t

--- js/go_game/go_act_page/go_game.js
+++ js/go_game/go_act_page/go_game.js
@@ -22,124 +22,8 @@ function GoPlayGameObject(root_object_val) {
         this.debug(false, "init__", "");
     };
 
-    this.objectName = function () {
-        return "GoPlayGameObject";
-    };
-
-    this.rootObject = function () {
-        return this.theRootObject;
-    };
-
-    this.phwangObject = function () {
-        return this.rootObject().phwangObject();
-    };
-
-    this.phwangLinkObject = function () {
-        return this.phwangObject().phwangLinkObject();
-    };
-
-    this.phwangSessionObject = function () {
-        return this.phwangObject().phwangSessionObject();
-    };
-
-    this.storageObject = function () {
-        return this.rootObject().storageObject();
-    };
-
-    this.configObject = function () {
-        return this.rootObject().configObject();
-    };
-
-    this.boardObject = function () {
-        return this.rootObject().boardObject();
-    };
-
-    this.portObject = function () {
-        return this.rootObject().portObject();
-    };
-
-    this.sessionObject = function () {
-        return this.rootObject().sessionObject();
-    };
-
-    this.totalMoves = function () {
-        return this.theTotalMoves;
-    };
-
-    this.setTotalMoves = function (total_moves_val) {
-        this.theTotalMoves = total_moves_val;
-    };
-
-    this.nextColor = function () {
-        return this.theNextColor;
-    };
-
-    this.setNextColor = function (next_color_val) {
-        this.theNextColor = next_color_val;
-    };
-
-    this.gameIsOver = function () {
-        return this.theGameIsOver;
-    };
-
-    this.setGameIsOver = function () {
-        this.theGameIsOver = true;
-    };
-
-    this.clearGameIsOver = function () {
-        this.theGameIsOver = false;
-    };
-
-    this.lastDeadX = function () {
-        return this.theLastDeadX;
-    };
-
-    this.setLastDeadX = function (val) {
-        this.theLastDeadX = val;
-    };
-
-    this.lastDeadY = function () {
-        return this.theLastDeadY;
-    };
-
-    this.setLastDeadY = function (val) {
-        this.theLastDeadY = val;
-    };
-
-    this.validLastDeadInfo = function () {
-        return this.theValidLastDeadInfo;
-    };
-
-    this.setValidLastDeadInfo = function (val) {
-        this.theValidLastDeadInfo = val;
-    };
-
-    this.blackScoreString = function () {
-        return this.theBlackScoreString;
-    }
-
-    this.setBlackScoreString = function (val) {
-        this.theBlackScoreString = val;
-    }
-
-    this.whiteScoreString = function () {
-        return this.theWhiteScoreString;
-    }
-
-    this.setWhiteScoreString = function (val) {
-        this.theWhiteScoreString = val;
-    }
-
-    this.finalScoreString = function () {
-        return this.theFinalScoreString;
-    }
-
-    this.setFinalScoreString = function (val) {
-        this.theFinalScoreString = val;
-    }
-
     this.enterGameFromUi = function (x_val, y_val) {
-        this.debug(true, "enterGameFromUi", "(" + x_val + "," + y_val + ")");
+        this.debug(false, "enterGameFromUi", "(" + x_val + "," + y_val + ")");
 
         if (this.gameIsOver()) {
             var move = new GoMoveObject(null, x_val, y_val, GO.THE_MARK_DEAD_STONE_DIFF, this.totalMoves(), this.containerObject());
@@ -155,42 +39,6 @@ function GoPlayGameObject(root_object_val) {
 
     };
 
-    this.processDoubleBackwardMoveFromUi = function () {
-        this.portObject().transmitSpecialMoveData(GO.DOUBLE_BACKWARD_MOVE());
-    };
-
-    this.processBackwardMoveFromUi = function () {
-        this.portObject().transmitSpecialMoveData(GO.BACKWARD_MOVE());
-    };
-
-    this.processForwardMoveFromUi = function () {
-        this.portObject().transmitSpecialMoveData(GO.FORWARD_MOVE());
-    };
-
-    this.processDoubleForwardMoveFromUi = function () {
-        this.portObject().transmitSpecialMoveData(GO.DOUBLE_FORWARD_MOVE());
-    };
-
-    this.processPassMoveFromUi = function () {
-        this.portObject().transmitSpecialMoveData(GO.PASS_MOVE());
-    };
-
-    this.processConfirmMoveFromUi = function () {
-        this.portObject().transmitSpecialMoveData(GO.CONFIRM_MOVE());
-    };
-
-    this.processResignMoveFromUi = function () {
-        this.portObject().transmitSpecialMoveData(GO.RESIGN_MOVE());
-    };
-
-    this.processPlayAnotherGameMoveFromUi = function () {
-        this.portObject().transmitSpecialMoveData(GO.PLAY_ANOTHER_GAME_MOVE());
-    };
-
-    this.processBackToPlayMoveFromUi = function () {
-        this.portObject().transmitSpecialMoveData(GO.BACK_TO_PLAY_MOVE());
-    };
-
     this.isValidMoveOnBoard = function (x_val, y_val) {
         if (this.boardObject().boardArray(x_val, y_val) !== GO.EMPTY_STONE()) {
             return false;
@@ -203,7 +51,7 @@ function GoPlayGameObject(root_object_val) {
     };
 
     this.playBothSides = function () {
-        this.logit("playBothSides", "myName=" + this.phwangLinkObject().myName() + " hisName=" + this.phwangSessionObject().hisName());
+        this.debug(true, "playBothSides", "myName=" + this.phwangLinkObject().myName() + " hisName=" + this.phwangSessionObject().hisName());
         return (this.phwangLinkObject().myName() === this.phwangSessionObject().hisName());
     };
 
@@ -220,20 +68,49 @@ function GoPlayGameObject(root_object_val) {
         }
     };
 
-    this.debug = function (debug_val, str1_val, str2_val) {
-        if (debug_val) {
-            this.logit(str1_val, str2_val);
-        }
-    };
+    this.processDoubleBackwardMoveFromUi = function () {this.portObject().transmitSpecialMoveData(GO.DOUBLE_BACKWARD_MOVE());};
+    this.processBackwardMoveFromUi = function () {this.portObject().transmitSpecialMoveData(GO.BACKWARD_MOVE());};
+    this.processForwardMoveFromUi = function () {this.portObject().transmitSpecialMoveData(GO.FORWARD_MOVE());};
+    this.processDoubleForwardMoveFromUi = function () {this.portObject().transmitSpecialMoveData(GO.DOUBLE_FORWARD_MOVE());};
+    this.processPassMoveFromUi = function () {this.portObject().transmitSpecialMoveData(GO.PASS_MOVE());};
+    this.processConfirmMoveFromUi = function () {this.portObject().transmitSpecialMoveData(GO.CONFIRM_MOVE());};
+    this.processResignMoveFromUi = function () {this.portObject().transmitSpecialMoveData(GO.RESIGN_MOVE());};
+    this.processPlayAnotherGameMoveFromUi = function () {this.portObject().transmitSpecialMoveData(GO.PLAY_ANOTHER_GAME_MOVE());};
+    this.processBackToPlayMoveFromUi = function () {this.portObject().transmitSpecialMoveData(GO.BACK_TO_PLAY_MOVE());};
 
-    this.logit = function (str1_val, str2_val) {
-        this.rootObject().logit_(this.objectName() + "." + str1_val, str2_val);
-    };
+    this.totalMoves = function () {return this.theTotalMoves;};
+    this.setTotalMoves = function (total_moves_val) {this.theTotalMoves = total_moves_val;};
+    this.nextColor = function () {return this.theNextColor;};
+    this.setNextColor = function (next_color_val) {this.theNextColor = next_color_val;};
+    this.gameIsOver = function () {return this.theGameIsOver;};
+    this.setGameIsOver = function () {this.theGameIsOver = true;};
+    this.clearGameIsOver = function () {this.theGameIsOver = false;};
+    this.lastDeadX = function () {return this.theLastDeadX;};
+    this.setLastDeadX = function (val) {this.theLastDeadX = val;};
+    this.lastDeadY = function () {return this.theLastDeadY;};
+    this.setLastDeadY = function (val) {this.theLastDeadY = val;};
+    this.validLastDeadInfo = function () {return this.theValidLastDeadInfo;};
+    this.setValidLastDeadInfo = function (val) {this.theValidLastDeadInfo = val;};
+    this.blackScoreString = function () {return this.theBlackScoreString;}
+    this.setBlackScoreString = function (val) {this.theBlackScoreString = val;}
+    this.whiteScoreString = function () {return this.theWhiteScoreString;}
+    this.setWhiteScoreString = function (val) {this.theWhiteScoreString = val;}
+    this.finalScoreString = function () {return this.theFinalScoreString;}
+    this.setFinalScoreString = function (val) {this.theFinalScoreString = val;}
 
-    this.abend = function (str1_val, str2_val) {
-        this.rootObject().abend_(this.objectName() + "." + str1_val, str2_val);
-    };
-
+    this.objectName = function () {return "GoPlayGameObject";};
+    this.rootObject = function () {return this.theRootObject;};
+    this.phwangObject = function () {return this.rootObject().phwangObject();};
+    this.phwangLinkObject = function () {return this.phwangObject().phwangLinkObject();};
+    this.phwangSessionObject = function () {return this.phwangObject().phwangSessionObject();};
+    this.storageObject = function () {return this.rootObject().storageObject();};
+    this.configObject = function () {return this.rootObject().configObject();};
+    this.boardObject = function () {return this.rootObject().boardObject();};
+    this.portObject = function () {return this.rootObject().portObject();};
+    this.sessionObject = function () {return this.rootObject().sessionObject();};
+    this.debug = function (debug_val, str1_val, str2_val) {if (debug_val) {this.logit(str1_val, str2_val);}};
+    this.logit = function (str1_val, str2_val) {this.rootObject().logit_(this.objectName() + "." + str1_val, str2_val);};
+    this.abend = function (str1_val, str2_val) {this.rootObject().abend_(this.objectName() + "." + str1_val, str2_val);};
     this.init__(root_object_val);
 }
 
