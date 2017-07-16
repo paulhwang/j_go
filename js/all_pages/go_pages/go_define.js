aeb116c62f0f973186eebadbfbbc8e4a818cc5c9
paulhwang <phwang@gmail.com>
2017-07-16 02:18:31 +0000

    t

--- js/all_pages/go_pages/go_define.js
+++ js/all_pages/go_pages/go_define.js
@@ -1,7 +1,6 @@
 /*
  * Copyrights phwang
  * Written by Paul Hwang
- * File name: GoDefineObject.js
  */
 
 var GO = new GoDefineObject;
@@ -16,32 +15,4 @@ function GoDefineObject() {
     this.MARKED_DEAD_WHITE_STONE = function () {return this.WHITE_STONE() + this.MARK_DEAD_STONE_DIFF();};
     this.MARKED_EMPTY_BLACK_STONE = function () {return this.BLACK_STONE() + this.MARK_EMPTY_STONE_DIFF();};
     this.MARKED_EMPTY_WHITE_STONE = function () {return this.WHITE_STONE() + this.MARK_EMPTY_STONE_DIFF();};
-
-    this.getOppositeColor = function (color_val) {
-        switch (color_val) {
-        case GO.BLACK_STONE():
-            return GO.WHITE_STONE();
-
-        case GO.WHITE_STONE():
-            return GO.BLACK_STONE();
-
-        default:
-            this.goAbend("getOppositeColor", "color=" + color_val);
-            return GO.EMPTY_STONE();
-        }
-    };
-
-    this.isNeighborStone = function (x1_val, y1_val, x2_val, y2_val) {
-        if (x1_val === x2_val) {
-            if ((y1_val + 1 === y2_val) || (y1_val - 1 === y2_val)) {
-                return true;
-            }
-        }
-        if (y1_val === y2_val) {
-            if ((x1_val + 1 === x2_val) || (x1_val - 1 === x2_val)) {
-                return true;
-            }
-        }
-        return false;
-    };
 }
