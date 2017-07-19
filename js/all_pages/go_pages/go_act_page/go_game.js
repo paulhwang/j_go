98fa48a2dab08c36c4340e3e4fc3024de6cb86aa
paulhwang <phwang@gmail.com>
2017-07-19 13:12:59 +0000

    t

--- js/all_pages/go_pages/go_act_page/go_game.js
+++ js/all_pages/go_pages/go_act_page/go_game.js
@@ -303,15 +303,10 @@ function GoPlayBoardObject(root_val) {
         }
     };
     this.resetBoardObjectData = function() {
-        var i, j;
-        i = 0;
-        while (i < this.boardSize()) {
-            j = 0;
-            while (j < this.boardSize()) {
+        for (var i = 0; i < this.boardSize(); i++) {
+            for (var j = 0; j < this.boardSize(); j++) {
                 this.setBoardArray(i, j, GO.EMPTY_STONE());
-                j += 1;
             }
-            i += 1;
         }
     };
     this.boardSize = function() {return this.configObject().boardSize();};
