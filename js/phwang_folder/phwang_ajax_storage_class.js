b34f60b7b8133c77fa129150119021ab0b85c45f
paulhwang <phwang@gmail.com>
2017-07-19 05:33:18 +0000

    t

--- js/phwang_folder/phwang_ajax_storage_class.js
+++ js/phwang_folder/phwang_ajax_storage_class.js
@@ -2,35 +2,3 @@
   Copyrights reserved
   Written by Paul Hwang
 */
-
-function PhwangAjaxStorageObject (phwang_ajax_object_val) {
-    "use strict";
-    this.storage = function () {return localStorage;};
-
-    this.init__ = function (phwang_ajax_object_val) {
-        this.thePhwangAjaxObject = phwang_ajax_object_val;
-        this.resetAjaxStorage();
-    };
-
-    this.resetAjaxStorage = function () {
-        this.resetAjaxPacketId();
-    };
-    this.resetAjaxPacketId = function () {if (this.ajaxPacketId() === undefined) {this.storage().ajax_packet_id = 0;}};
-
-    this.ajaxPacketId = function () {return this.storage().ajax_packet_id;};
-    this.incrementAjaxPacketId = function () {
-        var i = Number(this.storage().ajax_packet_id) + 1;
-        if (i !== 1 + Number(this.storage().ajax_packet_id)) {
-            this.abend("incrementAjaxPacketId", "fix it");
-        }
-        this.storage().ajax_packet_id = i;
-    };
-
-    this.objectName = function () {return "PhwangAjaxStorageObject";};
-    this.phwangAjaxObject = function () {return this.thePhwangAjaxObject;};
-    this.phwangObject = function () {return this.phwangAjaxObject().phwangObject();};
-    this.debug = function (debug_val, str1_val, str2_val) {if (debug_val) {this.logit(str1_val, str2_val);}};
-    this.logit = function (str1_val, str2_val) {return this.phwangObject().LOG_IT(this.objectName() + "." + str1_val, str2_val);};
-    this.abend = function (str1_val, str2_val) {return this.phwangObject().ABEND(this.objectName() + "." + str1_val, str2_val);};
-    this.init__(phwang_ajax_object_val);
-}
