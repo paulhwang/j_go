fafd38420eb6337166a6f198deea78c28b080f3e
Paul Hwang <phwang@gmail.com>
2022-09-13 09:33:46 -0700

    def

--- js/all_pages/define_pages/fe_define.js
+++ js/all_pages/define_pages/fe_define.js
@@ -9,14 +9,20 @@ function FE_DEFINE_OBJECT() {
         this.theFE_RESULT_SUCCEED                    = "00";
         this.theFE_RESULT_ACCOUNT_NAME_NOT_EXIST     = "01";
         this.theFE_RESULT_ACCOUNT_NAME_ALREADY_EXIST = "02";
-        this.theFE_RESULT_PASSWORD_NOT_MATCH          = "05";
-        this.theFE_RESULT_LINK_NOT_EXIST             = "11";
-        this.theFE_RESULT_SESSION_NOT_EXIST          = "12";
+        this.theFE_RESULT_PASSWORD_NOT_MATCH         = "05";
+
+        this.theFE_RESULT_DB_SELECT_FAIL             = "21";
+
+        this.theFE_RESULT_LINK_NOT_EXIST             = "51";
+        this.theFE_RESULT_SESSION_NOT_EXIST          = "52";
     };
     this.FE_RESULT_SUCCEED                    = function() {return this.theFE_RESULT_SUCCEED;};
     this.FE_RESULT_ACCOUNT_NAME_NOT_EXIST     = function() {return this.theFE_RESULT_ACCOUNT_NAME_NOT_EXIST;};
     this.FE_RESULT_ACCOUNT_NAME_ALREADY_EXIST = function() {return this.theFE_RESULT_ACCOUNT_NAME_ALREADY_EXIST;};
     this.FE_RESULT_PASSWORD_NOT_MATCH         = function() {return this.theFE_RESULT_PASSWORD_NOT_MATCH;};
+
+    this.FE_RESULT_DB_SELECT_FAIL             = function() {return this.theFE_RESULT_DB_SELECT_FAIL;};
+
     this.FE_RESULT_LINK_NOT_EXIST             = function() {return this.theFE_RESULT_LINK_NOT_EXIST;};
     this.FE_RESULT_SESSION_NOT_EXIST          = function() {return this.theFE_RESULT_SESSION_NOT_EXIST;};
 
