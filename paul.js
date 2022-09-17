0dd11c6ca9178827ffa2f7a0fd745fd4fad30322
Paul Hwang <phwang@gmail.com>
2022-09-17 07:57:37 -0700

    paul

--- paul.js
+++ paul.js
@@ -1,32 +1,34 @@
 
-function HwangRootObject() {
-    const go_button = document.querySelector(".go_button_class");
-    go_button.addEventListener("click", function() {
+function PaulObject() {
+    document.querySelector(".go_button").addEventListener("click", function() {
         window.open("go_config.html", "_self");
     });
 
-    const image_button = document.querySelector(".image_button_class");
-    image_button.addEventListener("click", function() {
+    document.querySelector(".image_button").addEventListener("click", function() {
         window.open("image.html", "_self");
     });
 
-    const mmw_button = document.querySelector(".mmw_button_class");
-    mmw_button.addEventListener("click", function() {
+    document.querySelector(".mmw_button").addEventListener("click", function() {
         window.open("mmw.html", "_self");
     });
 
-    const sign_up_button = document.querySelector(".sign_up_button_class");
-    sign_up_button.addEventListener("click", function() {
+    document.querySelector(".sign_up_button").addEventListener("click", function() {
         window.open("sign_up.html", "_self");
     });
 
-    const sign_in_button = document.querySelector(".sign_in_button_class");
-    sign_in_button.addEventListener("click", function() {
+    document.querySelector(".sign_in_button").addEventListener("click", function() {
         window.open("go_login.html", "_self");
+    });
+
+    document.querySelector(".sign_off_button").addEventListener("click", function() {
+        window.open("sign_off", "_self");
     });
 };
 
+var count = 0;
 document.addEventListener("DOMContentLoaded", function(){
-    new HwangRootObject();
+    console.log("count=" + count);
+    count++;
+    new PaulObject();
 });
 
