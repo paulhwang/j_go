7aa09dc44d81ee6a9bf94f5664def79364cb6932
Paul Hwang <phwang@gmail.com>
2022-09-17 10:46:24 -0700

    su

--- js/all_pages/account_pages/sign_up_root.js
+++ js/all_pages/account_pages/sign_up_root.js
@@ -5,101 +5,125 @@
 
 function SignUpRootObject() {
     "use strict";
+    this.ajaxRoute = function() {return "/django_go/go_ajax/";};
+    this.jsonContext = function() {return "application/json; charset=utf-8";}
+    this.plainTextContext = function() {return "text/plain; charset=utf-8";}
+
     this.init__ = function() {
-        this.thePhwangObject = new PhwangClass(this);
         this.theFE_DEF = new FE_DEFINE_OBJECT();
-        this.phwangObject().initObject();
-        this.theAjaxObject = new SignUpAjaxClass(this);
-        this.theHtmlObject = new SignUpHtmlObject(this);
-        this.debug(true, "init__", this.objectName());
+        this.thePhwangAjaxStorageObject = new PhwangAjaxStorageObject(this);
+
+        this.theHttpGetRequest = new XMLHttpRequest();
+        this.startAjaxWaiting();
+
+        this.bindHtmlInput();
     };
 
-    this.objectName = function() {return "SignUpRootObject";};
-    this.FE_DEF = function() {return this.theFE_DEF;};
-    this.phwangObject = function() {return this.thePhwangObject;};
-    this.phwangAjaxObject = function() {return this.phwangObject().phwangAjaxObject();};
-    this.htmlObject = function() {return this.theHtmlObject;};
-    this.ajaxObject = function() {return this.theAjaxObject;};
-    this.debug = function(debug_val, str1_val, str2_val) {if (debug_val) {this.logit(str1_val, str2_val);}};
-    this.logit = function(str1_val, str2_val) {return this.logit_(this.objectName() + "." + str1_val, str2_val);};
-    this.abend = function(str1_val, str2_val) {return this.abend_(this.objectName() + "." + str1_val, str2_val);};
-    this.logit_ = function(str1_val, str2_val) {this.phwangObject().LOG_IT(str1_val, str2_val);};
-    this.abend_ = function(str1_val, str2_val) {this.phwangObject().ABEND(str1_val, str2_val);};
-    this.init__();
-}
-
-function SignUpHtmlObject(root_object_val) {
-    "use strict";
-    this.gotoSignInPage = function() {window.open(this.phwangObject().serverHttpHeader() + "go_login.html", "_self")};
-
-    this.init__ = function(root_object_val) {
-        this.theRootObject = root_object_val; this.setupHtmlInput();
-    };
-
-    this.setupHtmlInput = function() {
+    this.bindHtmlInput = function() {
         var this0 = this;
         $(".sign_up_section .sign_up_button").on("click", function() {
             var account_name = $(".sign_up_section .sign_up_account_name").val();
             var password = $(".sign_up_section .sign_up_password").val();
             var email = $(".sign_up_section .sign_up_email").val();
-            this0.debug(true, "setupHtmlInput", "account_name=" + account_name + " password=" + password + " email=" + email);
             if (account_name) {
-                this0.phwangAjaxObject().signUpRequest(account_name, password, email);
+                var output = JSON.stringify({
+                        command: "sign_up",
+                        packet_id: this0.ajaxPacketId(),
+                        my_name: account_name,
+                        password: password,
+                        email: email,
+                        });
+                console.log("signUpRequest=" + output);
+
+                this0.sendAjaxRequest(output);
             }
         });
     };
 
-    this.objectName = function() {return "SignUpHtmlObject";};
-    this.rootObject = function() {return this.theRootObject;};
-    this.phwangObject = function() {return this.rootObject().phwangObject();};
-    this.phwangAjaxObject = function() {return this.phwangObject().phwangAjaxObject();};
-    this.ajaxObject = function() {return this.rootObject().ajaxObject();};
-    this.debug = function(debug_val, str1_val, str2_val) {if (debug_val) {this.logit(str1_val, str2_val);}};
-    this.logit = function(str1_val, str2_val) {return this.rootObject().logit_(this.objectName() + "." + str1_val, str2_val);};
-    this.abend = function(str1_val, str2_val) {return this.rootObject().abend_(this.objectName() + "." + str1_val, str2_val);};
-    this.init__(root_object_val);
-}
-
-function SignUpAjaxClass(root_object_val) {
-    "use strict";
-    this.init__ = function(root_object_val) {
-        this.theRootObject = root_object_val;
+    this.sendAjaxRequest = function(output_val) {
+        this.httpGetRequest().open("GET", this.ajaxRoute(), true);
+        this.httpGetRequest().setRequestHeader("X-Requested-With", "XMLHttpRequest");
+        this.httpGetRequest().setRequestHeader("Content-Type", this.jsonContext());
+        this.httpGetRequest().setRequestHeader("phwangajaxrequest", output_val);
+        this.httpGetRequest().setRequestHeader("phwangajaxpacketid", this.ajaxPacketId());
+        this.incrementAjaxPacketId();
+        this.httpGetRequest().send(null);
     };
 
-    this.receiveSignUpResponse = function(result_val) {
-        this.debug(true, "receiveSignUpResponse", "result_val=" + result_val);
-        var data = JSON.parse(result_val);
+    this.startAjaxWaiting = function() {
+        var this0 = this;
+        this.httpGetRequest().onreadystatechange = function() {
+            if ((this0.httpGetRequest().readyState === 4) &&
+                (this0.httpGetRequest().status === 200)) {
+                this0.examineResponse(this0.httpGetRequest().responseText);
+            }
+        };
+    };
+
+    this.examineResponse = function(json_response_val) {
+        console.log("json_response_val=" + json_response_val);
+
+        var response = JSON.parse(json_response_val);
+        console.log("response.data=" + response.data);
+
+        var data = JSON.parse(response.data);
         if (data.result === this.FE_DEF().FE_RESULT_SUCCEED()) {
-            this.debug(true, "receiveSignUpResponse", "succeed");
-            this.htmlObject().gotoSignInPage();
+            console.log("succeed");
+            this.gotoSignInPage();
         }
         else if (data.result === this.FE_DEF().FE_RESULT_ACCOUNT_NAME_ALREADY_EXIST()) {
-            this.debug(true, "receiveSignUpResponse", "account_name_already_exist");
+            console.log("account_name_already_exist");
         }
         else {
-            this.abend("receiveSignUpResponse", "invalid_result=" + data.result);
+            console.log("invalid_result=" + data.result);
         }
     };
 
-    /*
-    this.receiveSetupLinkResponse = function(result_val) {};
-    this.receiveSetupSessionResponse = function(result_val) {};
-    this.receiveSetupSession2Response = function(result_val) {};
-    this.receiveSetupSession3Response = function(result_val) {};
-    this.receivePutSessionDataResponse = function(result_val) {};
-    this.receiveGetSessionDataResponse = function(result_val, data_val) {};
-    */
+    this.gotoSignInPage = function() {
+        //window.open(this.phwangObject().serverHttpHeader() + "sign_in.html", "_self")
+        window.open("go_login.html", "_self")
+    };
 
-    this.objectName = function() {return "SignUpAjaxClass";};
-    this.rootObject = function() {return this.theRootObject;};
-    this.FE_DEF = function() {return this.rootObject().FE_DEF();};
-    this.phwangObject = function() {return this.rootObject().phwangObject();};
-    this.phwangLinkObject = function() {return this.rootObject().phwangLinkObject();};
-    this.htmlObject = function() {return this.rootObject().htmlObject();};
+    this.FE_DEF = function() {return this.theFE_DEF;};
+    this.httpGetRequest = function() {return this.theHttpGetRequest;};
+    this.ajaxPacketId = function() {return this.phwangAjaxObject().ajaxPacketId();};
+    this.phwangAjaxStorageObject = function() {return this.thePhwangAjaxStorageObject;};
+    this.ajaxPacketId = function() {return this.phwangAjaxStorageObject().ajaxPacketId();};
+    this.incrementAjaxPacketId = function() {this.phwangAjaxStorageObject().incrementAjaxPacketId();};
+    this.init__();
+}
+
+function PhwangAjaxStorageObject(phwang_ajax_object_val) {
+    "use strict";
+    this.storage = function() {return sessionStorage;};
+    this.init__ = function (phwang_ajax_object_val) {
+        this.thePhwangAjaxObject = phwang_ajax_object_val;
+        this.resetAjaxStorage();
+    };
+
+    this.resetAjaxStorage = function() {
+        this.resetAjaxPacketId();
+    };
+
+    this.resetAjaxPacketId = function() {if (this.ajaxPacketId() === undefined) {
+        this.storage().ajax_packet_id = 0;}
+    };
+    this.ajaxPacketId = function() {return this.storage().ajax_packet_id;};
+    this.incrementAjaxPacketId = function() {
+        var i = Number(this.storage().ajax_packet_id) + 1;
+        if (i !== 1 + Number(this.storage().ajax_packet_id)) {
+            this.abend("incrementAjaxPacketId", "fix it");
+        }
+        this.storage().ajax_packet_id = i;
+    };
+
+    this.objectName = function() {return "PhwangAjaxStorageObject";};
+    this.phwangAjaxObject = function() {return this.thePhwangAjaxObject;};
+    this.phwangObject = function() {return this.phwangAjaxObject().phwangObject();};
     this.debug = function(debug_val, str1_val, str2_val) {if (debug_val) {this.logit(str1_val, str2_val);}};
-    this.logit = function(str1_val, str2_val) {return this.rootObject().logit_(this.objectName() + "." + str1_val, str2_val);};
-    this.abend = function(str1_val, str2_val) {return this.rootObject().abend_(this.objectName() + "." + str1_val, str2_val);};
-    this.init__(root_object_val);
+    this.logit = function(str1_val, str2_val) {return this.phwangObject().LOG_IT(this.objectName() + "." + str1_val, str2_val);};
+    this.abend = function(str1_val, str2_val) {return this.phwangObject().ABEND(this.objectName() + "." + str1_val, str2_val);};
+    this.init__(phwang_ajax_object_val);
 }
 
 var sign_up_main = function() {"use strict"; new SignUpRootObject();};
