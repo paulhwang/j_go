c72ffe25d2316141bc30e7b18738f248a883674d
Paul Hwang <phwang@gmail.com>
2022-09-29 13:38:53 -0700

    board

--- js/app_dir/go_dir/go_play_dir/go_play_board.js
+++ js/app_dir/go_dir/go_play_dir/go_play_board.js
@@ -6,16 +6,16 @@
 function GoPlayBoardObject(root_val) {
     "use strict";
     this.init__ = function (root_val) {
-        this.theRootObject = root_val;
-        this.theBoardArray = [19];
-        this.theMarkedBoardArray = [19];
+        this.rootObject_ = root_val;
+        this.boardArray_ = [19];
+        this.markedBoardArray_ = [19];
         for (var i = 0; i < 19; i++) {
-            this.theBoardArray[i] = [19];
-            this.theMarkedBoardArray[i] = [19];
+            this.boardArray_[i] = [19];
+            this.markedBoardArray_[i] = [19];
         }
         this.resetBoardObjectData();
-        this.debug(false, "init__", "");
     };
+    /*
     this.addStoneToBoard = function(x_val, y_val, color_val) {
         if (!GO.isValidCoordinates(x_val, y_val, this.configObject().boardSize())) {
             this.goAbend("addStoneToBoard", "x=" + x_val + " y=" + y_val);
@@ -46,6 +46,7 @@ function GoPlayBoardObject(root_val) {
         }
         return true;
     };
+    */
     this.decodeBoard = function(str_val) {
         var index = 0;
         var num;
@@ -57,23 +58,27 @@ function GoPlayBoardObject(root_val) {
         this.gameObject().setNextColor(num);
         for (var i = 0; i < this.boardSize(); i++) {
             for (var j = 0; j < this.boardSize(); j++) {
-                this.theBoardArray[i][j] = str_val.charAt(index++) - '0';
+                this.boardArray_[i][j] = str_val.charAt(index++) - '0';
             }
         }
         num  = (str_val.charAt(index++) - '0') * 100;
         num += (str_val.charAt(index++) - '0') * 10;
         num += (str_val.charAt(index++) - '0');
-        this.theBlackCapturedStones = num;
+        this.blackCapturedStones_ = num;
+
         num  = (str_val.charAt(index++) - '0') * 100;
         num += (str_val.charAt(index++) - '0') * 10;
         num += (str_val.charAt(index++) - '0');
-        this.theWhiteCapturedStones = num;
+        this.whiteCapturedStones_ = num;
+
         num  = (str_val.charAt(index++) - '0') * 10;
         num += (str_val.charAt(index++) - '0');
         this.gameObject().setLastDeadX(num);
+
         num  = (str_val.charAt(index++) - '0') * 10;
         num += (str_val.charAt(index++) - '0');
         this.gameObject().setLastDeadY(num);
+
         if (num != 19) {
             this.gameObject().setValidLastDeadInfo(true);
         } else {
@@ -94,19 +99,18 @@ function GoPlayBoardObject(root_val) {
             }
         }
     };
-    this.boardSize = function() {return this.configObject().boardSize();};
-    this.blackCapturedStones = function() {return this.theBlackCapturedStones;};
-    this.whiteCapturedStones = function() {return this.theWhiteCapturedStones;};
-    this.boardArray = function (x_val, y_val) {return this.theBoardArray[x_val][y_val];};
-    this.markedBoardArray = function (x_val, y_val) {return this.theMarkedBoardArray[x_val][y_val];};
-    this.setBoardArray = function (x_val, y_val, data_val) {this.theBoardArray[x_val][y_val] = data_val;};
-    this.setMarkedBoardArray = function (x_val, y_val, data_val) {this.theMarkedBoardArray[x_val][y_val] = data_val;};
-    this.objectName = function() {return "GoPlayBoardObject";};
-    this.rootObject = function() {return this.theRootObject;};
-    this.configObject = function() {return this.rootObject().configObject();};
-    this.gameObject = function() {return this.rootObject().gameObject();};
-    this.debug = function(debug_val, str1_val, str2_val) {if (debug_val) {this.logit(str1_val, str2_val);}};
-    this.logit = function(str1_val, str2_val) {this.rootObject().logit_(this.objectName() + "." + str1_val, str2_val);};
-    this.abend = function(str1_val, str2_val) {this.rootObject().abend_(this.objectName() + "." + str1_val, str2_val);};
+
+    this.boardSize = () => this.configObject().boardSize();
+    this.blackCapturedStones = () => this.blackCapturedStones_;
+    this.whiteCapturedStones = () => this.whiteCapturedStones_;
+    this.boardArray = (x_val, y_val) => this.boardArray_[x_val][y_val];
+
+    this.markedBoardArray = function (x_val, y_val) {return this.markedBoardArray_[x_val][y_val];};
+    this.setBoardArray = function (x_val, y_val, data_val) {this.boardArray_[x_val][y_val] = data_val;};
+    this.setMarkedBoardArray = function (x_val, y_val, data_val) {this.markedBoardArray_[x_val][y_val] = data_val;};
+
+    this.rootObject = () => this.rootObject_;
+    this.configObject = () => this.rootObject().configObject();
+    this.gameObject = () => this.rootObject().gameObject();
     this.init__(root_val);
 }
