ae266d3b3df6907c60c6c8526942a5777e77e0ff
Paul Hwang <phwang@gmail.com>
2022-09-28 20:45:02 -0700

    new

---
+++ js/app_dir/go_dir/go_base_dir/go_base_fabric.js
@@ -0,0 +1,11 @@
+/*
+  Copyrights reserved
+  Written by Paul Hwang
+*/
+
+function GoBaseFabricObject() {
+    this.init__ = function(go_base_root_object_val) {
+        this.theGoBaseRootObject = go_base_root_object_val;
+    };
+    this.init__();
+};
