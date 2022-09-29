3d1e4fe3e4cdf2bd4c28e038f8f87f32897838ae
Paul Hwang <phwang@gmail.com>
2022-09-29 13:59:06 -0700

    game

--- js/app_dir/go_dir/go_play_dir/go_play_game.js
+++ js/app_dir/go_dir/go_play_dir/go_play_game.js
@@ -6,7 +6,7 @@
 function GoPlayGameObject(root_object_val) {
     "use strict";
     this.init__ = function(root_object_val) {
-        this.theRootObject = root_object_val;
+        this.rootObject_ = root_object_val;
         this.theBlackCaptureStones = 0;
         this.theWhiteCaptureStones = 0;
         this.theLastDeadX = 0;
@@ -19,17 +19,21 @@ function GoPlayGameObject(root_object_val) {
         this.theNextColor = GO.BLACK_STONE();
         this.theGameIsOver = false;
 
-        if (sessionStorage.my_name === sessionStorage.peer_name) {
-            this.thePlayBothSides = true;
+        this.setPlayBothsides();
+    }
+
+    this.setPlayBothsides = () => {
+        if (this.linkObject().myName() === this.sessionObject().peerName()) {
+            this.playBothSides_ = true;
         }
         else {
-            this.thePlayBothSides = false;
+            this.playBothSides_ = false;
         }
-        console.log("GoPlayGameObject.init__() thePlayBothSides=" + this.thePlayBothSides);
+        console.log("GoPlayGameObject.setPlayBothsides() playBothSides=" + this.playBothSides());
     };
 
     this.processNewMove = function(x_val, y_val) {
-        this.debug(false, "processNewMove", "(" + x_val + "," + y_val + ")");
+        console.log("GoPlayGameObject.processNewMove() (" + x_val + "," + y_val + ")");
         if (this.gameIsOver()) {
             var move = new GoMoveObject(null, x_val, y_val, GO.THE_MARK_DEAD_STONE_DIFF, this.totalMoves(), this.containerObject());
             this.portObject().transmitMoveData(move);
@@ -41,6 +45,7 @@ function GoPlayGameObject(root_object_val) {
         var move = new GoMoveObject(null, x_val, y_val, this.nextColor(), this.totalMoves() + 1, this.rootObject());
         this.portObject().transmitMoveData(move);
     };
+
     this.isValidMoveOnBoard = function(x_val, y_val) {
         if (this.boardObject().boardArray(x_val, y_val) !== GO.EMPTY_STONE()) {
             return false;
@@ -50,23 +55,14 @@ function GoPlayGameObject(root_object_val) {
         }
         return true;
     };
-    this.playBothSides = function() {
-        return this.thePlayBothSides;
-        /*
-        this.debug(false, "playBothSides", "myName=" + this.phwangLinkObject().myName() + " hisName=" + this.configObject().hisName());
-        var result = (this.phwangLinkObject().myName() === this.configObject().hisName());
-        if (this.configObject().playBothSides() !== result) {
-            this.abend("playBothSides", "not match");
-        }
-        return result;
-        */
-    };
+
     this.isMyTurn = function() {
-        //console.log("GoPlayGameObject.isMyTurn() nextColor=" + this.nextColor() + ", myColor=" + this.configObject().myColor());
         var result = (this.playBothSides()) || (this.nextColor() === this.configObject().myColor());
-        //console.log("GoPlayGameObject.isMyTurn() result=" + result);
         return result;
     };
+
+    this.playBothSides = () => this.playBothSides_;
+
     this.totalMoves = function() {return this.theTotalMoves;};
     this.setTotalMoves = function(total_moves_val) {this.theTotalMoves = total_moves_val;};
     this.nextColor = function() {return this.theNextColor;};
@@ -86,18 +82,13 @@ function GoPlayGameObject(root_object_val) {
     this.setWhiteScoreString = function(val) {this.theWhiteScoreString = val;}
     this.finalScoreString = function() {return this.theFinalScoreString;}
     this.setFinalScoreString = function(val) {this.theFinalScoreString = val;}
-    this.objectName = function() {return "GoPlayGameObject";};
-    this.rootObject = function() {return this.theRootObject;};
-    this.phwangObject = function() {return this.rootObject().phwangObject();};
-    this.phwangLinkObject = function() {return this.rootObject().phwangLinkObject();};
-    this.phwangSessionObject = function() {return this.phwangObject().phwangSessionObject();};
-    this.storageObject = function() {return this.rootObject().storageObject();};
-    this.configObject = function() {return this.rootObject().configObject();};
-    this.boardObject = function() {return this.rootObject().boardObject();};
-    this.portObject = function() {return this.rootObject().portObject();};
-    this.sessionObject = function() {return this.rootObject().sessionObject();};
-    this.debug = function(debug_val, str1_val, str2_val) {if (debug_val) {this.logit(str1_val, str2_val);}};
-    this.logit = function(str1_val, str2_val) {this.rootObject().logit_(this.objectName() + "." + str1_val, str2_val);};
-    this.abend = function(str1_val, str2_val) {this.rootObject().abend_(this.objectName() + "." + str1_val, str2_val);};
+
+    this.rootObject = () => this.rootObject_;
+    this.storageObject = () => this.rootObject().storageObject();
+    this.configObject = () => this.rootObject().configObject();
+    this.boardObject = () => this.rootObject().boardObject();
+    this.portObject = () => this.rootObject().portObject();
+    this.linkObject = () => this.rootObject().linkObject();
+    this.sessionObject = () => this.rootObject().sessionObject();
     this.init__(root_object_val);
-}
+};
