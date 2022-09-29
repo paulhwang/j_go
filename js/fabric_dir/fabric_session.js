9413ab7fc8a7c870e96a9bdf9ca41f580b59a236
Paul Hwang <phwang@gmail.com>
2022-09-28 23:02:26 -0700

    session

--- js/fabric_dir/fabric_session.js
+++ js/fabric_dir/fabric_session.js
@@ -5,7 +5,42 @@
 
 function FabricSessionObject() {
     this.init__ = function() {
+        this.getSessionInfoFromStorage();
     }
+
+    this.getSessionInfoFromStorage = function() {
+        this.sessionId_ = sessionStorage.getItem("session_id");
+        if (this.sessionId_ === null) {
+            console.log("FabricSessionObject.getSessionInfoFromStorage() null session_id");
+            abend();
+            return -1;
+        }
+        this.groupMode = sessionStorage.getItem("group_mode");
+        if (this.groupMode === undefined) {
+            console.log("FabricSessionObject.getSessionInfoFromStorage() undefined group_mode");
+            abend();
+            return -1;
+        }
+        this.themeData = sessionStorage.getItem("go_config_data");
+        if (this.themeData === null) {
+            console.log("FabricSessionObject.getSessionInfoFromStorage() null go_config_data");
+            abend();
+            return -1;
+        }
+        this.initiatorName = sessionStorage.getItem("initiator_name");
+        if (this.initiatorName === null) {
+            console.log("FabricSessionObject.getSessionInfoFromStorage() null initiator_name");
+            abend();
+            return -1;
+        }
+        this.peerName = sessionStorage.getItem("peer_name");
+        if (this.peerName === null) {
+            console.log("FabricSessionObject.getSessionInfoFromStorage() null peer_name");
+            abend();
+            return -1;
+        }
+        return 0;
+    };
 
     this.setSessionInfoIntoStorage = function(session_id_val, group_mode_val, theme_data_val, initiator_name_val, peer_name_val) {
         sessionStorage.setItem("session_id", session_id_val);
@@ -15,8 +50,10 @@ function FabricSessionObject() {
         sessionStorage.setItem("peer_name", peer_name_val);
     };
 
-    this.linkId = () => this.linkId_;
-    this.myName = () => this.myName_;
-    this.timeStamp = () => this.timeStamp_;
+    this.sessionId = () => this.sessionId_;
+    this.groupMode = () => this.groupMode_;
+    this.themeData = () => this.themeData_;
+    this.initiatorName = () => this.initiatorName_;
+    this.peerName = () => this.peerName_;
     this.init__();
 };
