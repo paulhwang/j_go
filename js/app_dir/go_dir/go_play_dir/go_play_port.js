ba9e20bc9e77003ccff32e0075d6ee73630c8b23
Paul Hwang <phwang@gmail.com>
2022-09-29 14:07:01 -0700

    port

--- js/app_dir/go_dir/go_play_dir/go_play_port.js
+++ js/app_dir/go_dir/go_play_dir/go_play_port.js
@@ -17,7 +17,11 @@ function GoPlayPortObject(root_val) {
     this.GO_PROTOCOL_RESIGN_COMMAND = function() {return "R";};
     this.GO_PROTOCOL_CONFIRM_COMMAND = function() {return "C";};
     this.GO_PROTOCOL_CONTINUE_COMMAND = function() {return "c";};
-    this.init__ = function(root_val) {this.theRootObject = root_val;};
+
+    this.init__ = function(root_val) {
+      this.rootObject_ = root_val;
+    };
+
     this.transmitMoveData = function(move_val) {this.transmitGameData(this.GO_PROTOCOL_MOVE_COMMAND() + move_val.encodeMove());};
     this.transmitPassCommand = function() {this.transmitGameData(this.GO_PROTOCOL_PASS_COMMAND());};
     this.transmitBackwardCommand = function() {this.transmitGameData(this.GO_PROTOCOL_BACKWARD_COMMAND());};
@@ -27,18 +31,20 @@ function GoPlayPortObject(root_val) {
     this.transmitResignCommand = function() {this.transmitGameData(this.GO_PROTOCOL_RESIGN_COMMAND());};
     this.transmitConfirmCommand = function() {this.transmitGameData(this.GO_PROTOCOL_CONFIRM_COMMAND());};
     this.transmitContinueCommand = function() {this.transmitGameData(this.GO_PROTOCOL_CONTINUE_COMMAND());};
+
     this.transmitGameData = function(data_val) {
-        this.debug(false, "transmitGameData", "data_val=" + data_val);
+        console.log("GoPlayPortObject.transmitGameData() data_val=" + data_val);
         this.inputObject().setPendingRequestExist();
         this.transmitData(this.GO_PROTOCOL_GAME_INFO() + data_val);
     };
+
     this.transmitData = function(data_val) {
-        this.debug(true, "transmitData", "data_val=" + data_val);
+        console.log("GoPlayPortObject.transmitData() data_val=" + data_val);
         this.rootObject().fabricObject().sendPutSessionDataRequest(data_val);
-        //this.phwangSessionObject().transmitData(data_val);
     };
+
     this.receiveData = function(data_val) {
-        this.debug(true, "receiveData", "data_val=" + data_val);
+        console.log("GoPlayPortObject.receiveData() data_val=" + data_val);
         if (data_val.charAt(0) === this.GO_PROTOCOL_GAME_INFO().charAt(0)) {
             this.receiveGameData(data_val.slice(1));
             return;
@@ -49,24 +55,19 @@ function GoPlayPortObject(root_val) {
         if (data_val.charAt(0) === this.GO_PROTOCOL_CHAT_INFO().charAt(0)) {
             return;
         }
-        this.abend("receiveData", "data_val=" + data_val);
     };
+
     this.receiveGameData = function(data_val) {
         this.inputObject().clearPendingRequestExist();
         this.boardObject().decodeBoard(data_val);
         this.displayObject().drawBoard();
     };
-    this.objectName = function() {return "GoPlayPortObject";};
-    this.rootObject = function() {return this.theRootObject;};
-    this.ajxObject = function() {return this.rootObject().ajxObject();};
-    this.configObject = function() {return this.rootObject().configObject(); };
-    this.gameObject = function() {return this.rootObject().gameObject();};
-    this.inputObject = function() {return this.rootObject().inputObject();};
-    this.phwangSessionObject = function() {return this.rootObject().phwangSessionObject();};
-    this.boardObject = function() {return this.rootObject().boardObject();};
-    this.displayObject = function() {return this.rootObject().displayObject();};
-    this.debug = function (debug_val, str1_val, str2_val) {if (debug_val) {this.logit(str1_val, str2_val);}};
-    this.logit = function (str1_val, str2_val) {this.rootObject().logit_(this.objectName() + "." + str1_val, str2_val);};
-    this.abend = function (str1_val, str2_val) {this.rootObject().abend_(this.objectName() + "." + str1_val, str2_val);};
+
+    this.rootObject = () => this.rootObject_;
+    this.configObject = () => this.rootObject().configObject();
+    this.gameObject = () => this.rootObject().gameObject();
+    this.inputObject = () => this.rootObject().inputObject();
+    this.boardObject = () => this.rootObject().boardObject();
+    this.displayObject = () => this.rootObject().displayObject();
     this.init__(root_val);
 }
