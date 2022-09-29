015b4ffc75e8f2566799fa40395fef81a87f7c58
Paul Hwang <phwang@gmail.com>
2022-09-29 11:45:26 -0700

    config

--- js/app_dir/go_dir/go_config_entry.js
+++ js/app_dir/go_dir/go_config_entry.js
@@ -3,7 +3,7 @@
  * Written by Paul Hwang
  */
 
-function GoPlayConfigObject(root_val) {
+function GoConfigObject(root_val) {
     "use strict";
     this.init__ = function(root_val) {
         this.theRootObject = root_val;
@@ -81,34 +81,6 @@ function GoPlayConfigObject(root_val) {
     this.debug = function(debug_val, str1_val, str2_val) {if (debug_val) {this.logit(str1_val, str2_val);}};
     this.logit = function(str1_val, str2_val) {this.rootObject().logit_(this.objectName() + "." + str1_val, str2_val);};
     this.abend = function(str1_val, str2_val) {this.rootObject().abend_(this.objectName() + "." + str1_val, str2_val);};
-    this.init__(root_val);
-}
-
-function GoConfigStorageObject(root_val) {
-    "use strict";
-    this.init__ = function (root_val) {
-        this.theRootObject = root_val;
-        this.debug(true, "init__", "myColor=" + this.myColor() + " boardSize=" + this.boardSize() + " hisName=" + this.hisName() + " handicapPoint=" + this.handicapPoint() + " komiPoint=" + this.komiPoint());
-    };
-    this.storage = function() {return sessionStorage;};
-    this.myColor = function() {return Number(this.storage().go_my_color);};
-    this.setMyColor = function(val) {this.storage().go_my_color = val;};
-    this.hisColor = function() {if (this.myColor() === GO.BLACK_STONE()) {return GO.WHITE_STONE();} else {return GO.BLACK_STONE();}};
-    this.hisName = function() {return this.storage().go_his_name;};
-    this.setHisName = function(val) {this.storage().go_his_name = val;};
-    this.boardSize = function() {return Number(this.storage().go_board_size);};
-    this.setBoardSize = function(val) {this.storage().go_board_size = val;};
-    this.handicapPoint = function() {return Number(this.storage().go_handicap_point);};
-    this.setHandicapPoint = function(val) {this.storage().go_handicap_point = val;};
-    this.komiPoint = function() {return Number(this.storage().go_komi_point);};
-    this.setKomiPoint = function (val) {this.storage().go_komi_point = val;};
-    this.goEncodedConfig = function() {return this.storage().go_encoded_config;};
-    this.setGoEncodedConfig = function(val) {this.storage().go_encoded_config = val;};
-    this.objectName = function() {return "GoConfigStorageObject";};
-    this.rootObject = function() {return this.theRootObject;};
-    this.debug = function (debug_val, str1_val, str2_val) {if (debug_val) {this.logit(str1_val, str2_val);}};
-    this.logit = function (str1_val, str2_val) {this.rootObject().logit_(this.objectName() + "." + str1_val, str2_val);};
-    this.abend = function (str1_val, str2_val) {this.rootObject().abend_(this.objectName() + "." + str1_val, str2_val);};
     this.init__(root_val);
 }
 
--- js/app_dir/go_dir/go_play_dir/go_play_root.js
+++ js/app_dir/go_dir/go_play_dir/go_play_root.js
@@ -8,20 +8,21 @@ function GoPlayRootObject() {
     this.init__ = function() {
         this.linkObject_ = new FabricLinkObject();
         this.linkObject().getLinkInfoFromStorage();
+
         this.sessionObject_ = new FabricSessionObject();
         this.sessionObject().getSessionInfoFromStorage();
 
+        this.configObject_ = new GoConfigObject(this);
+        this.configObject().decodeGoConfig();
 
-        this.theConfigObject = new GoPlayConfigObject(this);
-        this.theConfigObject.decodeGoConfig();
         this.htmlObject_ = new GoPlayHtmlObject(this);
         this.fabricObject_ = new GoPlayFabricObject(this);
-        this.theHtmlObject = new GoPlayHtmlObject(this);
-        this.theInputObject = new GoPlayInputObject(this);
-        this.theBoardObject = new GoPlayBoardObject(this);
-        this.thePortObject = new GoPlayPortObject(this);
-        this.theGameObject = new GoPlayGameObject(this);
-        this.theDisplayObject = new GoPlayDisplayObject(this);
+        this.htmlObject_ = new GoPlayHtmlObject(this);
+        this.inputObject_ = new GoPlayInputObject(this);
+        this.boardObject_ = new GoPlayBoardObject(this);
+        this.portObject_ = new GoPlayPortObject(this);
+        this.gameObject_ = new GoPlayGameObject(this);
+        this.displayObject_ = new GoPlayDisplayObject(this);
 
 /*
 
@@ -70,40 +71,18 @@ function GoPlayRootObject() {
     };
 
 
-    this.configObject = function() {return this.theConfigObject;};
-    this.boardObject = function() {return this.theBoardObject;};
-    this.portObject = function() {return this.thePortObject;};
-    this.gameObject = function() {return this.theGameObject;};
-    this.inputObject = function() {return this.theInputObject;};
-    this.displayObject = function() {return this.theDisplayObject;};
-    this.htmlObject = function() {return this.theHtmlObject;};
-
-
+    this.configObject = () => this.configObject_;
+    this.boardObject = () => this.boardObject_;
+    this.portObject = () => this.portObject_;
+    this.gameObject = () => this.gameObject_
+    this.inputObject = () => this.inputObject_;
+    this.displayObject = () => this.displayObject_;
+    this.htmlObject = () => this.htmlObject_;
     this.linkObject = () => this.linkObject_;
     this.sessionObject = () => this.sessionObject_;
     this.fabricObject = () => this.fabricObject_;
     this.htmlObject = () => this.htmlObject_;
     this.init__();
-}
-
-function GoAjaxClass(root_object_val) {
-    "use strict";
-    this.init__ = function (root_object_val) {this.theRootObject = root_object_val;};
-    this.receiveSetupLinkResponse = function(result_val) {};
-    this.receiveGetNameListResponse = function(result_val) {};
-    this.receiveSetupSessionResponse = function(result_val) {};
-    this.receiveSetupSession2Response = function(result_val) {};
-    this.receiveSetupSession3Response = function(result_val) {};
-    this.receivePutSessionDataResponse = function (result_val) {};
-    this.receiveGetSessionDataResponse = function (result_val, data_val) {};
-    this.objectName = function() {return "GoAjaxClass";};
-    this.rootObject = function() {return this.theRootObject;};
-    this.phwangObject = function() {return this.rootObject().phwangObject();};
-    this.htmlObject = function() {return this.rootObject().htmlObject();};
-    this.debug = function(debug_val, str1_val, str2_val) {if (debug_val) {this.logit(str1_val, str2_val);}};
-    this.logit = function(str1_val, str2_val) {return this.phwangObject().LOG_IT(this.objectName() + "." + str1_val, str2_val);};
-    this.abend = function(str1_val, str2_val) {return this.phwangObject().ABEND(this.objectName() + "." + str1_val, str2_val);};
-    this.init__(root_object_val);
 }
 
 var go_play_main = function() {"use strict"; new GoPlayRootObject();};
