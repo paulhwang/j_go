99ee54ebbd28b970786e0f85004b2567b9b8391f
Paul Hwang <phwang@gmail.com>
2022-10-04 12:49:43 -0700

    go

--- js/app_dir/go_dir/go_play_dir/go_play_root.js
+++ js/app_dir/go_dir/go_play_dir/go_play_root.js
@@ -7,30 +7,32 @@ function GoPlayRootObject() {
     "use strict";
     this.init__ = function() {
         this.fabricSessionPutGetObject_ = new FabricSessionPutGetObject();
+
         this.configObject_ = new GoConfigObject(this);
         this.configObject().decode();
         this.configObject().printConfigInfo();
-        this.htmlObject_ = new GoPlayHtmlObject(this);
-        this.uiMouseObject_ = new GoPlayUiMouseObject(this);
-        this.boardObject_ = new GoPlayBoardObject(this);
-        this.portObject_ = new GoPlayPortObject(this);
-        this.gameObject_ = new GoPlayGameObject(this);
+
+        this.htmlObject_      = new GoPlayHtmlObject(this);
+        this.uiMouseObject_   = new GoPlayUiMouseObject(this);
+        this.boardObject_     = new GoPlayBoardObject(this);
+        this.portObject_      = new GoPlayPortObject(this);
+        this.gameObject_      = new GoPlayGameObject(this);
         this.uiDisplayObject_ = new GoPlayUiDisplayObject(this);
     };
 
-    this.configObject = () => this.configObject_;
-    this.boardObject = () => this.boardObject_;
-    this.portObject = () => this.portObject_;
-    this.gameObject = () => this.gameObject_
-    this.uiMouseObject = () => this.uiMouseObject_;
-    this.uiDisplayObject = () => this.uiDisplayObject_;
-    this.htmlObject = () => this.htmlObject_;
-    this.linkObject = () => this.fabricSessionPutGetObject().linkObject();
-    this.sessionObject = () => this.fabricSessionPutGetObject().sessionObject();
     this.fabricSessionPutGetObject = () => this.fabricSessionPutGetObject_;
-    this.htmlObject = () => this.htmlObject_;
+    this.linkObject                = () => this.fabricSessionPutGetObject().linkObject();
+    this.sessionObject             = () => this.fabricSessionPutGetObject().sessionObject();
+
+    this.configObject    = () => this.configObject_;
+    this.boardObject     = () => this.boardObject_;
+    this.portObject      = () => this.portObject_;
+    this.gameObject      = () => this.gameObject_
+    this.uiMouseObject   = () => this.uiMouseObject_;
+    this.uiDisplayObject = () => this.uiDisplayObject_;
+    this.htmlObject      = () => this.htmlObject_;
+
     this.init__();
 }
 
-var go_play_main = function() {"use strict"; new GoPlayRootObject();};
-$(document).ready(go_play_main);
+$(document).ready(() => {new GoPlayRootObject();});
