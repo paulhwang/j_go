5d422def5a1db55fd71267007d0818bed81ab4e3
Paul Hwang <phwang@gmail.com>
2022-10-04 13:28:54 -0700

    session

--- js/app_dir/go_dir/go_base_dir/go_base_fabric.js
+++ js/app_dir/go_dir/go_base_dir/go_base_fabric.js
@@ -7,6 +7,12 @@ function FabricSessionSetupObject(root_object_val) {
     "use strict";
      this.init__ = function(root_object_val) {
         this.rootObject_ = root_object_val;
+
+        this.linkObject_ = new FabricLinkObject();
+        this.linkObject().getLinkInfoFromStorage();
+
+        this.sessionObject_ = new FabricSessionObject();
+
         this.httpServiceObject_ = new HttpServiceObject(this.examineResponse, this);
     };
 
@@ -56,8 +62,8 @@ function FabricSessionSetupObject(root_object_val) {
     };
 
     this.rootObject = () => this.rootObject_;
-    this.linkObject = () => this.rootObject().linkObject();
-    this.sessionObject = () => this.rootObject().sessionObject();
+    this.linkObject = () => this.linkObject_;
+    this.sessionObject = () => this.sessionObject_;
     this.httpServiceObject = () => this.httpServiceObject_;
     this.init__(root_object_val);
 };
--- js/app_dir/go_dir/go_base_dir/go_base_root.js
+++ js/app_dir/go_dir/go_base_dir/go_base_root.js
@@ -6,19 +6,16 @@
 function GoBaseRootObject() {
     "use strict";
     this.init__ = function() {
-        this.linkObject_ = new FabricLinkObject();
-        this.linkObject().getLinkInfoFromStorage();
-        this.sessionObject_ = new FabricSessionObject();
-        this.htmlObject_ = new GoBaseHtmlObject(this);
+
         this.fabricSessionSetupObject_ = new FabricSessionSetupObject(this);
+        this.htmlObject_ = new GoBaseHtmlObject(this);
     };
 
-    this.linkObject = () => this.linkObject_;
-    this.sessionObject = () => this.sessionObject_;
     this.fabricSessionSetupObject = () => this.fabricSessionSetupObject_;
+    this.linkObject = () => this.fabricSessionSetupObject().linkObject();
+    this.sessionObject = () => this.fabricSessionSetupObject().sessionObject_;
     this.htmlObject = () => this.htmlObject_;
     this.init__();
 };
 
-var go_base_main = function() {"use strict"; new GoBaseRootObject();};
 $(document).ready(() => {new GoBaseRootObject();});
