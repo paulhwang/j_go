1080c3666f5edc935dd765a958039dc97d7bbe38
Paul Hwang <phwang@gmail.com>
2022-10-06 12:15:18 -0700

    fabric

--- js/app_dir/go_dir/go_duet_dir/go_duet_html.js
+++ js/app_dir/go_dir/go_duet_dir/go_duet_html.js
@@ -19,7 +19,7 @@ function GoDuetHtmlObject(root_object_val) {
             let board_size = $(".config_section .go_config_section .board_size").val();
             let komi = $(".config_section .go_config_section .komi").val();
             let handicap = $(".config_section .go_config_section .handicap").val();
-            let theme_data = this0.encodeGoConfig(board_size, handicap, komi, initiator_color);
+            const theme_data = this0.encodeGoConfig(board_size, handicap, komi, initiator_color);
             console.log("GoDuetHtmlObject.setupHtmlInputFun() board_size=" + board_size + " initiator_color=" + initiator_color + " komi=" + komi + " handicap=" + handicap + " myName=" + this0.linkObject().myName() + " second_fiddle=" + second_fiddle + " theme_data=" + theme_data);
             this0.portObject().transmitData(FE_DEF.THEME_IS_GO_GAME(), theme_data, FE_DEF.GROUP_MODE_DUET(), this0.linkObject().myName());
         });
--- js/fabric_dir/fabric_session_setup.js
+++ js/fabric_dir/fabric_session_setup.js
@@ -13,7 +13,8 @@ function FabricSessionSetupObject(root_object_val) {
 
         this.sessionObject_ = new FabricSessionObject();
 
-        this.httpServiceObject_ = new FabricHttpServiceObject(this.examineResponse, this);
+        this.initResponseProcessSwitchTable();
+        this.httpServiceObject_ = new FabricHttpServiceObject(this.parseAndSwitchFabricResponse, this);
         this.sendGetLinkDataRequest();
     };
 
@@ -22,67 +23,32 @@ function FabricSessionSetupObject(root_object_val) {
         this.setupSessionCallbackObject_ = object_val;
     };
 
-    this.examineResponse = function(json_response_val) {
-        console.log("FabricSessionSetupObject.examineResponse() json_response_val=" + json_response_val);
+    this.initResponseProcessSwitchTable = function() {
+        this.responseSwitchTable_ = {
+            "get_link_data": this.processGetLinkDataResponse,
+            "get_name_list": this.processGetNameListResponse,
+            "setup_session": this.processSetupSessionResponse,
+            "setup_session2": this.processSetupSession2Response,
+            "setup_session3": this.processSetupSession3Response,
+        };
+    };
+
+    this.parseAndSwitchFabricResponse = function(json_response_val) {
+        console.log("FabricSessionSetupObject.parseAndSwitchFabricResponse() json_response_val=" + json_response_val);
         const response = JSON.parse(json_response_val);
 
-        console.log("FabricSessionSetupObject.examineResponse() response.data=" + response.data);
+        console.log("FabricSessionSetupObject.parseAndSwitchFabricResponse() response.data=" + response.data);
         const data = JSON.parse(response.data);
 
-        if (response.command === "get_link_data") {
-            if (data.result === FE_DEF.RESULT_SUCCEED()) {
-
-                //this.sendGetLinkDataRequest();
-            }
-            else {
-                //tbd
-            }
-        }
-
-        else if (response.command === "setup_session") {
-            if (data.result === FE_DEF.RESULT_SUCCEED()) {
-                console.log("FabricSessionSetupObject.examineResponse(setup_session) succeed! session_id=", data.session_id);
-                if (data.room_status === FE_DEF.ROOM_STATUS_READY()) {
-                    this.sessionObject().setSessionInfoIntoStorage(data.session_id, data.group_mode, data.theme_type, data.theme_data, data.first_fiddle, data.second_fiddle);
-                    this.setupSessionCallbackFunc().bind(this.setupSessionCallbackObject())();
-                }
-                else {
-                }
-            }
-            else if (data.result === FE_DEF.RESULT_ALMOST_SUCCEED()) {
-                console.log("FabricSessionSetupObject.examineResponse(setup_session) almost_succeed");
-                this.sendSetupSession3Request(data.session_id);
-            }
-            else if (data.result === FE_DEF.RESULT_ACCOUNT_NAME_NOT_EXIST()) {
-                console.log("FabricSessionSetupObject.examineResponse(setup_session) account_not_exist");
-            }
-            else {
-                console.log("FabricSessionSetupObject.examineResponse(setup_session) invalid_result=" + data.result);
-            }
-        }
-
-        else if (response.command === "setup_session3") {
-            if (data.result === FE_DEF.RESULT_SUCCEED()) {
-                console.log("FabricSessionSetupObject.examineResponse(setup_session3) succeed! session_id=", data.session_id);
-                if (data.room_status === FE_DEF.ROOM_STATUS_READY()) {
-                    this.sessionObject().setSessionInfoIntoStorage(data.session_id, data.group_mode, data.theme_type, data.theme_data, data.first_fiddle, data.second_fiddle);
-                    this.setupSessionCallbackFunc().bind(this.setupSessionCallbackObject())();
-                }
-                else {
-                }
-            }
-            else if (data.result === FE_DEF.RESULT_ALMOST_SUCCEED()) {
-                console.log("FabricSessionSetupObject.examineResponse(setup_session3) almost_succeed");
-                this.sendSetupSession3Request(data.session_id);
-            }
-            else {
-                console.log("FabricSessionSetupObject.examineResponse(setup_session) invalid_result=" + data.result);
-            }
-        }
-
-        else {
+        var func = this.responseSwitchTable()[response.command];
+        if (!func) {
+            console.log("FabricSessionSetupObject.parseAndSwitchFabricResponse() bad_command=" + response.command);
             abend();
+            return;
         }
+
+        this.clearPendingAjaxRequestCommand();
+        func.bind(this)(data);
     };
 
     this.sendGetLinkDataRequest = function() {
@@ -93,6 +59,25 @@ function FabricSessionSetupObject(root_object_val) {
                 });
         console.log("FabricSessionSetupObject.sendGetLinkDataRequest() output=" + output);
         this.httpServiceObject().sendAjaxRequest(output); 
+    };
+
+    this.processGetLinkDataResponse = function(data_val) {
+
+    };
+
+    this.sendGetNameListRequest = function(name_list_tag_val) {
+        var output = JSON.stringify({
+                command: "get_name_list",
+                time_stamp: link_val.timeStamp(),
+                link_id: this.linkObject().linkId(),
+                name_list_tag: name_list_tag_val,
+                });
+        console.log("FabricSessionSetupObject.sendGetNameListRequest() output=" + output);
+        this.httpServiceObject().sendAjaxRequest(output); 
+    };
+
+    this.processGetNameListResponse = function(data_val) {
+
     };
 
     this.sendSetupSessionRequest = function(theme_type_val, theme_data_val, group_mode_val, second_fiddle_val) {
@@ -110,6 +95,28 @@ function FabricSessionSetupObject(root_object_val) {
         this.httpServiceObject().sendAjaxRequest(output); 
     };
 
+    this.processSetupSessionResponse = function(data_val) {
+        if (data_val.result === FE_DEF.RESULT_SUCCEED()) {
+            console.log("FabricSessionSetupObject.examineResponse(setup_session) succeed! session_id=", data_val.session_id);
+            if (data_val.room_status === FE_DEF.ROOM_STATUS_READY()) {
+                this.sessionObject().setSessionInfoIntoStorage(data_val.session_id, data.group_mode, data_val.theme_type, data_val.theme_data, data_val.first_fiddle, data_val.second_fiddle);
+                this.setupSessionCallbackFunc().bind(this.setupSessionCallbackObject())();
+            }
+            else {
+            }
+        }
+        else if (data_val.result === FE_DEF.RESULT_ALMOST_SUCCEED()) {
+            console.log("FabricSessionSetupObject.examineResponse(setup_session) almost_succeed");
+            this.sendSetupSession3Request(data_val.session_id);
+        }
+        else if (data_val.result === FE_DEF.RESULT_ACCOUNT_NAME_NOT_EXIST()) {
+            console.log("FabricSessionSetupObject.examineResponse(setup_session) account_not_exist");
+        }
+        else {
+            console.log("FabricSessionSetupObject.examineResponse(setup_session) invalid_result=" + data_val.result);
+        }
+    };
+
     this.sendSetupSession3Request = function(session_id_val) {
         const output = JSON.stringify({
                 command: "setup_session3",
@@ -121,7 +128,31 @@ function FabricSessionSetupObject(root_object_val) {
         this.httpServiceObject().sendAjaxRequest(output); 
     };
 
+    this.processSetupSession3Response = function(data_val) {
+        if (data_val.result === FE_DEF.RESULT_SUCCEED()) {
+            console.log("FabricSessionSetupObject.examineResponse(setup_session3) succeed! session_id=", data_val.session_id);
+            if (data_val.room_status === FE_DEF.ROOM_STATUS_READY()) {
+                this.sessionObject().setSessionInfoIntoStorage(data_val.session_id, data_val.group_mode, data_val.theme_type, data_val.theme_data, data_val.first_fiddle, data_val.second_fiddle);
+                this.setupSessionCallbackFunc().bind(this.setupSessionCallbackObject())();
+            }
+            else {
+            }
+        }
+        else if (data_val.result === FE_DEF.RESULT_ALMOST_SUCCEED()) {
+            console.log("FabricSessionSetupObject.examineResponse(setup_session3) almost_succeed");
+            this.sendSetupSession3Request(data_val.session_id);
+        }
+        else {
+            console.log("FabricSessionSetupObject.examineResponse(setup_session) invalid_result=" + data_val.result);
+        }
+    };
+
+    this.clearPendingAjaxRequestCommand = function() {
+
+    };
+
     this.rootObject = () => this.rootObject_;
+    this.responseSwitchTable = () => this.responseSwitchTable_;
     this.linkObject = () => this.linkObject_;
     this.sessionObject = () => this.sessionObject_;
     this.setupSessionCallbackFunc = () => this.setupSessionCallbackFunc_;
