9ba59683368203073b24ff3c5d8f175412811c0c
Paul Hwang <phwang@gmail.com>
2022-10-07 09:19:22 -0700

    put

--- js/fabric_dir/fabric_session_put_get.js
+++ js/fabric_dir/fabric_session_put_get.js
@@ -30,34 +30,42 @@ function FabricSessionPutGetObject() {
 
         if (response.command === "put_session_data") {
             const data = JSON.parse(response.data);
-            if (data.result === FE_DEF.RESULT_SUCCEED()) {
-                console.log("FabricSessionPutGetObject.examineResponse(put_session_data) succeed! session_id=", data.session_id);
-                this.putCallbackFunc().bind(this.putCallbackObject())(data.result_data);
-            }
-            if (data.result === FE_DEF.RESULT_ALMOST_SUCCEED()) {
-                console.log("FabricSessionPutGetObject.examineResponse(put_session_data) in_progress! session_id=", data.session_id);
-                this.sendGetSessionDataRequest();
-            }
-            else {
-                console.log("FabricSessionPutGetObject.examineResponse(put_session_data) invalid_result=" + data.result);
-            }
+            this.processPutSessionDataResponse(data);
         }
         else if (response.command === "get_session_data") {
             const data = JSON.parse(response.data);
-            if (data.result === FE_DEF.RESULT_SUCCEED()) {
-                console.log("FabricSessionPutGetObject.examineResponse(get_session_data) succeed! session_id=", data.session_id);
-                this.putCallbackFunc().bind(this.putCallbackObject())(data.result_data);
-            }
-            if (data.result === FE_DEF.RESULT_ALMOST_SUCCEED()) {
-                console.log("FabricSessionPutGetObject.examineResponse(get_session_data) in_progress! session_id=", data.session_id);
-                this.sendGetSessionDataRequest();
-            }
-            else {
-                console.log("FabricSessionPutGetObject.examineResponse(get_session_data) invalid_result=" + data.result);
-            }
+            this.processGetSessionDataResponse(data);
         }
         else {
             abend();
+        }
+    };
+
+    this.processPutSessionDataResponse = function(data_val) {
+        if (data_val.result === FE_DEF.RESULT_SUCCEED()) {
+            console.log("FabricSessionPutGetObject.examineResponse(put_session_data) succeed! session_id=", data_val.session_id);
+            this.putCallbackFunc().bind(this.putCallbackObject())(data_val.result_data);
+        }
+        if (data_val.result === FE_DEF.RESULT_ALMOST_SUCCEED()) {
+            console.log("FabricSessionPutGetObject.examineResponse(put_session_data) in_progress! session_id=", data_val.session_id);
+            this.sendGetSessionDataRequest();
+        }
+        else {
+            console.log("FabricSessionPutGetObject.examineResponse(put_session_data) invalid_result=" + data_val.result);
+        }
+    };
+
+    this.processGetSessionDataResponse = function(data_val) {
+        if (data_val.result === FE_DEF.RESULT_SUCCEED()) {
+            console.log("FabricSessionPutGetObject.examineResponse(get_session_data) succeed! session_id=", data_val.session_id);
+            this.putCallbackFunc().bind(this.putCallbackObject())(data_val.result_data);
+        }
+        if (data_val.result === FE_DEF.RESULT_ALMOST_SUCCEED()) {
+            console.log("FabricSessionPutGetObject.examineResponse(get_session_data) in_progress! session_id=", data_val.session_id);
+            this.sendGetSessionDataRequest();
+        }
+        else {
+            console.log("FabricSessionPutGetObject.examineResponse(get_session_data) invalid_result=" + data_val.result);
         }
     };
 
