29d0df4f1cf7bb73a4f5c1328c835ac8e29c674c
Paul Hwang <phwang@gmail.com>
2022-10-13 10:27:55 -0700

    base

--- js/fabric_dir/fabric_base.js
+++ js/fabric_dir/fabric_base.js
@@ -7,14 +7,14 @@ function FabricBaseObject(root_obj_val) {
     "use strict";
      this.init__ = function(root_obj_val) {
         this.rootObj_ = root_obj_val;
-        this.fabricResponseObj_ = new FabricResponseObject(this);
-        this.fabricRequestObj_ = new FabricRequestObject(this);
+        this.dFabricObj_ = new DFabricObject(this);
+        this.uFabricObj_ = new UFabricObject(this);
     };
 
-    this.fabricResponseObject = () => this.fabricResponseObj_;
-    this.fabricRequestObject = () => this.fabricRequestObj_;
-    this.linkObj = () => this.fabricRequestObject().linkObj();
-    this.sessionObj = () => this.fabricRequestObject().sessionObj();
+    this.DFabricObj = () => this.dFabricObj_;
+    this.UFabricObj = () => this.uFabricObj_;
+    this.linkObj = () => this.UFabricObj().linkObj();
+    this.sessionObj = () => this.UFabricObj().sessionObj();
     this.configObj = () => this.configObj_;
     this.init__(root_obj_val);
 };
