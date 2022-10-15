7291dca97d58fb1e8bd449613cd67c6772d40caa
Paul Hwang <phwang@gmail.com>
2022-10-15 14:44:25 -0700

    modal

--- js/app_dir/dtf_dir/dtf_html.js
+++ js/app_dir/dtf_dir/dtf_html.js
@@ -1,3 +1,42 @@
+let modal;
+document.addEventListener("click", (e) => {
+  if (e.target.className === "modal-open") {
+    modal = document.getElementById(e.target.dataset.id);
+    openModal(modal);
+  } else if (e.target.className === "modal-close") {
+    closeModal(modal);
+  } else {
+    return;
+  }
+});
+
+const openModal = (modal) => {
+  document.body.style.overflow = "hidden";
+  modal.setAttribute("open", "true");
+  document.addEventListener("keydown", escClose);
+  let overlay = document.createElement("div");
+  overlay.id = "modal-overlay";
+  document.body.appendChild(overlay);
+};
+
+const closeModal = (modal) => {
+  document.body.style.overflow = "auto";
+  modal.removeAttribute("open");
+  document.removeEventListener("keydown", escClose);
+  document.body.removeChild(document.getElementById("modal-overlay"));
+};
+
+const escClose = (e) => {
+  if (e.keyCode == 27) {
+    closeModal();
+  }
+};
+
+
+
+
+
+
 /*
   Copyrights reserved
   Written by Paul Hwang
@@ -62,10 +101,34 @@ function DtfHtmlObject(root_object_val) {
         }
     }
 
+
+    this.openModal = (modal) => {
+        document.body.style.overflow = "hidden";
+        modal.setAttribute("open", "true");
+        document.addEventListener("keydown", escClose);
+        let overlay = document.createElement("div");
+        overlay.id = "modal-overlay";
+        document.body.appendChild(overlay);
+    };
+
+    this.closeModal = (modal) => {
+        document.body.style.overflow = "auto";
+        modal.removeAttribute("open");
+        document.removeEventListener("keydown", escClose);
+        document.body.removeChild(document.getElementById("modal-overlay"));
+    };
+
+    this.escClose = (e) => {
+        if (e.keyCode == 27) {
+            closeModal();
+        }
+    };
+
     this.setupKindsSelectors = () => {
         for (let i = 0; i < kind_items.length; i++) {
             document.querySelector(".kind" + ENCODE.encodeNumber(i, 2) + "_kind_button").addEventListener("click", function() {
-                window.open("go_solo.html", "_self");
+                //window.open("go_solo.html", "_self");
+                openModal(modal);
             });
        }
     };
