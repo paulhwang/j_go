7fe450010279a8697052cbca5da89e0e09eba056
Paul Hwang <phwang@gmail.com>
2022-10-21 12:08:09 -0700

    file

--- js/app_dir/dtf_dir/dtf_wd.js
+++ js/app_dir/dtf_dir/dtf_wd.js
@@ -10,7 +10,7 @@ function DtfWdObject(root_obj_val) {
         this.rootObj_ = root_obj_val;
         this.serviceQueue_ = new QueueClass(1);
 
-        let a = ["dtf.txt", "R", this.htmlObj().setKindItems];
+        let a = ["kind_items", "R", this.htmlObj().setKindItems];
         this.enqueueService(a);
     };
 
