57852ffb4cd7d75b1d7e5c7c6bb0436d8f799d28
Paul Hwang <phwang@gmail.com>
2022-10-22 11:21:00 -0700

    wd

--- js/app_dir/dtf_dir/dtf_wd.js
+++ js/app_dir/dtf_dir/dtf_wd.js
@@ -8,25 +8,23 @@ function DtfWdObject(root_obj_val) {
 
     this.init__ = (root_obj_val) => {
         this.rootObj_ = root_obj_val;
-        this.htmlQueue_ = new QueueClass(2);
+        this.htmlQue_ = new QueueClass(2);
         this.prepareReadHtml();
     };
 
     this.prepareReadHtml = () => {
-        this.htmlQueue().enqueueData(["preludes",   this.htmlObj().setPreludes]);
-        this.htmlQueue().enqueueData(["kind_items", this.htmlObj().setKindItems]);
+        this.htmlQue().enqueueData(["preludes",   this.htmlObj().setPreludes]);
+        this.htmlQue().enqueueData(["kind_items", this.htmlObj().setKindItems]);
     };
 
     this.doReadHtml = () => {
-        if (this.htmlQueue().queueLen() === 0) {
+        if (this.htmlQue().queueLen() === 0) {
             this.htmlObj().startHtmlObject();
         }
 
-        let block = this.htmlQueue().dequeueData();
-        if (block !== null) {
-            const file_name = block[0];
-            const callback_func = block[1];
-            this.portObj().readInfo(file_name, callback_func, this.doReadHtml);
+        const e = this.htmlQue().dequeueData();
+        if (e !== null) {
+            this.portObj().readInfo(e[0], e[1], this.doReadHtml);
         }
     };
 
@@ -36,6 +34,6 @@ function DtfWdObject(root_obj_val) {
     this.dFabricObj = () => this.rootObj().dFabricObj();
     this.uFabricObj = () => this.rootObj().uFabricObj();
     this.portObj = () => this.rootObj().portObj();
-    this.htmlQueue = () => this.htmlQueue_;
+    this.htmlQue = () => this.htmlQue_;
     this.init__(root_obj_val);
 }
