d44de15a90b4bbd9322ebf3c84e0d21c6d01988e
Paul Hwang <phwang@gmail.com>
2022-10-22 22:52:21 -0700

    wr

--- js/app_dir/dtf_dir/dtf_port.js
+++ js/app_dir/dtf_dir/dtf_port.js
@@ -52,12 +52,12 @@ function DtfPortObject(root_val) {
         }
         else if (cmd_val === FE_DEF.WRITE_FILE_RESPONSE()) {
             const fd = data1_val;
-            console.log("DtfPortObject.receiveFabricResponse() cmd=" + cmd_val + " fd=" + fd);
+            console.log("DtfPortObject.receiveFabricResponse(write) cmd=" + cmd_val + " fd=" + fd);
             this.wdObj().doWriteHtml(fd);
         }
         else if (cmd_val === FE_DEF.WRITE_MORE_FILE_RESPONSE()) {
             const fd = data1_val;
-            console.log("DtfPortObject.receiveFabricResponse() cmd=" + cmd_val + " fd=" + fd);
+            console.log("DtfPortObject.receiveFabricResponse(write_more) cmd=" + cmd_val + " fd=" + fd);
             this.wdObj().doWriteHtml(fd);
         }
         else if (cmd_val === FE_DEF.GET_NAME_LIST_RESPONSE()) {
--- js/fabric_dir/d_fabric.js
+++ js/fabric_dir/d_fabric.js
@@ -488,17 +488,12 @@ function DFabricObject(root_obj_val) {
         const result = data.slice(index, index + FE_DEF.RESULT_SIZE());
         index += FE_DEF.RESULT_SIZE();
 
-        const link_id = data.slice(index, index + FE_DEF.LINK_ID_SIZE());
-        index += FE_DEF.LINK_ID_SIZE();
-
-        const session_id = data.slice(index, index + FE_DEF.SESSION_ID_SIZE());
-        index += FE_DEF.SESSION_ID_SIZE();
-
-        const result_data = data.slice(index);
+        const fd = data.slice(index, index + FE_DEF.FD_LEN_SIZE());
+        index += FE_DEF.FD_LEN_SIZE();
 
         if (result === FE_DEF.RESULT_SUCCEED()) {
-            console.log("DFabricObject.writeMoreFileResponse() succeed!");
-            this.callbackFunc().bind(this.callbackObj())(cmd_val);
+            console.log("DFabricObject.writeMoreFileResponse() succeed! fd=" + fd);
+            this.callbackFunc().bind(this.callbackObj())(cmd_val, result, fd);
         }
         else {
             console.log("DFabricObject.writeMoreFileResponse() invalid_result=" + result);
